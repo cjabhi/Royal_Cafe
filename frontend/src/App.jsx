07b8c89c1838454473c53abee0b30d8de40755fc
Abhishek Sharma <raunakabhishek234@gmail.com>
2024-10-13 20:19:40 +0530

    jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar.jsx'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import Footer from './components/Footer/Footer'
-import LoginPopup from './components/LoginPopup/LoginPopup'
-import Verify from './pages/verify/verify'
-import MyOrders from './pages/MyOrders/MyOrders'
+import Cart from './pages/Cart/Cart.jsx'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import LoginPopup from './components/LoginPopup/LoginPopup.jsx'
+import Verify from './pages/verify/Verify.jsx'
+import MyOrders from './pages/MyOrders/MyOrders.jsx'
 
 const App = () => {
   
