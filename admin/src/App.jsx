a8dd72249b572fff4c48f7fa8489ce770c2d0d56
cjabhi <106903075+cjabhi@users.noreply.github.com>
2024-10-17 00:11:07 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000";
+  const url = "https://royal-cafe.onrender.com";
 
   return (
     <div>
